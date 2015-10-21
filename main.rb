5d5a4870536489e24f5d98b59697249c9f84c03d
John Shea <john@johnshea.nyc>
2015-10-21 16:20:47 -0400

    added Jon's menu code

--- main.rb
+++ main.rb
@@ -3,3 +3,25 @@ class Player
     @cards = []
   end
 end
+
+class Menu
+  def initialize
+    puts "1. New game!"
+    puts "2. Quit"
+    gets option.chomp
+    print "Option: "
+    if option == 1
+      puts "starting new game..."
+
+      #do something... like maybe...
+      #1. deal the cards to player and/or dealer
+      #2. draw card from pile...
+      #3. select card from deck... etc
+    elsif option == 2
+      puts "quitting..."
+      #quit the game
+    else
+      print "invalid option"
+    end
+  end
+end
