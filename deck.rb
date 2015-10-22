b6525e13d339582d5d340b8c91abf98b92b1f01d
Pin <pin@Lu-MacPro.local>
2015-10-22 12:23:31 -0400

    pile updated

--- deck.rb
+++ deck.rb
@@ -6,6 +6,7 @@ class Deck
   attr_writer :cards
   RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
   SUITS = ["Spades", "Hearts", "Clubs", "Diamonds"]
+
   def initialize
     @cards = []
 
@@ -16,38 +17,50 @@ class Deck
     end
     @cards.shuffle!
   end
+
   def draw(howMany = 1)
-    return @cards.pop(howMany)
+    return cards.pop(howMany)
   end
+
   def shuffle
     @cards.shuffle!
   end
+
   def return(card_to_return)
     @cards.unshift(card_to_return)
   end
 end
 
+def deal(num)
+   hand = []
+   num.times { hand << @cards.shift }
+   hand
+ end
+
 ###
+
+
 class Pile
 
-  attr_reader :cards
-  attr_writer :cards
-  
+  attr_reader :cards, :player
+
   def initialize
       @cards =[]
       puts "test"
-
   end
 
   def top_card
-
-   @cards = draw()
-
+    @deck = deck
+    @cards = Deck.draw
+    puts "this is getting into the top_card"
+    puts @cards
   end
-
 end
 
-
+# an array of @cards, starting as empty
+# a method called top_card which tells you the card which is face up
+# a method called validate_play which takes a card as a parameter, and returns true if it could be played on top of the pile
+# a method called add which takes a card and adds it to the pile
 
 
 # class Card
