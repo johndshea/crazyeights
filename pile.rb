45ebd1ca226fa543ed1a5879800efd96adc3ac7b
Pin <pin@Lu-MacPro.local>
2015-10-22 10:52:24 -0400

    pile.rb added

--- pile.rb
+++ pile.rb
@@ -1,3 +1,25 @@
 class pile
 
-  
+  def initialize
+      @cards =[]
+          RANKS.each do |rank|
+            SUITS.each do |suit|
+
+  end
+
+  def top_card
+    @cards = array[]
+
+  end
+
+
+
+
+end
+
+
+  #
+  # an array of @cards, starting as empty
+  # a method called top_card which tells you the card which is face up
+  # a method called validate_play which takes a card as a parameter, and returns true if it could be played on top of the pile
+  # a method called add which takes a card and adds it to the pile
